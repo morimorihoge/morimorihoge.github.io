9408bcfa085ddf308d7a0a6a855d957408264360
Masato Mori <masatomo.m@gmail.com>
2018-11-07 16:27:59 +0900

    中間課題様のTweet JS

---
+++ teu2018f/tweets.js
@@ -0,0 +1,14 @@
+// JavaScript中で使うための配列
+let tweets = [
+  { name: "三郎BOT", message: "21時でござる", tweetedAt: "2018-11-01 21:00:00" },
+  { name: "次郎", message: "悲しいので寝ます", tweetedAt: "2018-11-01 20:14:44" },
+  { name: "次郎", message: "33-4・・・・・", tweetedAt: "2018-11-01 20:13:03" },
+  { name: "太郎", message: "満腹度が回復した", tweetedAt: "2018-11-01 20:05:38" },
+  { name: "三郎BOT", message: "20時でござる", tweetedAt: "2018-11-01 20:00:00" },
+  { name: "太郎", message: "もぐもぐ・・・", tweetedAt: "2018-11-01 19:50:22" },
+  { name: "太郎", message: "ごはんつくった！", tweetedAt: "2018-11-01 19:48:01" },
+  { name: "太郎", message: "お腹空いたなあ....", tweetedAt: "2018-11-01 19:30:59" },
+  { name: "太郎", message: "お腹空いたなあ", tweetedAt: "2018-11-01 19:08:01" },
+  { name: "次郎", message: "久々にやきうやったるで！パワプロやけどな！", tweetedAt: "2018-11-01 19:04:12" },
+  { name: "三郎BOT", message: "19時でござる", tweetedAt: "2018-11-01 19:00:00" }
+];
