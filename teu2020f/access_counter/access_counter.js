2b3709a6c4e1f9bc10fa1e1fa3e37b97f6c174a4
Masato Mori <morimori@bpsinc.jp>
2021-01-06 18:36:47 +0900

    アクセスカウンター実装
    一通りの実装を完了

--- teu2020f/access_counter/access_counter.js
+++ teu2020f/access_counter/access_counter.js
@@ -1,6 +1,6 @@
 let sheetId = '1VwrGuz6_cJ-_M-qpjjCT1r6xIEyEFJw4omxdMJqNMsg';
 let sheetName = 'summary';
-let apiKey = 'AIzaSyDqV4k5Ygqa7SEtF30PRtvqXKlvRzdsvaE';
+let apiKey = 'AIzaSyDqV4k5Ygqa7SEtF30PRtvqXKlvRzdsvaE'; // only enabled in morimorihoge.github.io domain
 
 fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`).then(
     response => {
@@ -8,16 +8,13 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetNa
     }
 ).then(
     json => {
-      console.log(json);
-      console.log(json.values[1][1]);
       let counterElement = document.querySelector('#access-counter');
-
       let imgHtml = "";
 
       // 数値変換して1桁ずつIMGタグを作っていく
       let count = Number(json.values[1][1]);
 
-      for(let i = 0;i < 8;i++){
+      for (let i = 0; i < 8; i++) {
         // そのときのcountの1の位を抽出
         let digit = count % 10;
         imgHtml = `<img src="images/candle_number${digit}.png" alt="${digit}" class="counter">` + imgHtml;
